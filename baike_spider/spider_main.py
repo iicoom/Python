870d1407d423350d64419dda513b7ad3705d9630
Leo <asdfpeng@qq.com>
2018-01-27 00:19:52 +0800

    main

---
+++ baike_spider/spider_main.py
@@ -0,0 +1,9 @@
+class SpiderMain(object):
+    def __init__(self):
+        self.url = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        pass
